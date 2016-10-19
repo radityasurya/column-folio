f67ba76c45ed6b57cee31b090f181ace34838b93
radityasurya <radityasurya@hotmail.com>
2016-10-19 03:12:58 +0200

    Add tooltips to navbar

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,11 @@
-
-
 $(function() {
     $(".panels").mousewheel(function(event, delta) {
         this.scrollLeft -= (delta * 30);
         event.preventDefault();
     });
+});
+
+$('.tooltip').tooltipster({
+    side: 'right',
+    theme: 'tooltipster-shadow'
 });
