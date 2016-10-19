f94d39270b292e10c55ae1e131722faa6543be97
radityasurya <radityasurya@hotmail.com>
2016-10-19 15:02:01 +0200

    add page loader

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,13 @@ $('.tooltip').tooltipster({
     side: 'right',
     theme: 'tooltipster-shadow'
 });
+
+var nanobar = new Nanobar();
+
+//move bar
+nanobar.go(80);
+
+setTimeout(function() {
+    nanobar.go(100);
+    $('.fade').removeClass('out');
+}, 1000);
