086ab4ea3ff7f4e0a02f596698ffeb3e47167f63
radityasurya <radityasurya@hotmail.com>
2016-10-19 19:19:00 +0200

    Add scrollto plugins

--- app/js/main.js
+++ app/js/main.js
@@ -11,11 +11,17 @@ $('.tooltip').tooltipster({
 });
 
 var nanobar = new Nanobar();
-
 //move bar
 nanobar.go(80);
-
 setTimeout(function() {
     nanobar.go(100);
     $('.fade').removeClass('out');
 }, 1000);
+
+$('a.nav__link[href^="#"]').click(function(e) {
+    // Prevent the jump and the #hash from appearing on the address bar
+    e.preventDefault();
+    // Scroll the window, stop any previous animation, stop on user manual scroll
+    // Check https://github.com/flesler/jquery.scrollTo for more customizability
+    $('.panels').stop(true).scrollTo(this.hash, 700, { offset: { left: -20 } });
+});
